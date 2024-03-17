db194b0af0a35946758b53e8832c95fbb9fb6f5e
Aayush0169 <rajaayush124212@gmail.com>
2024-03-17 09:45:56 +0530

    First Commit

---
+++ PATTERNS/DynamiCrectanglE.cpp
@@ -0,0 +1,24 @@
+//Rectangle on user given inputs
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cout<<"(enter diffrent dimmensions for rectnagle)"<<endl;
+    cout<<"Enter the dimenssion of row"<<endl;
+    cin>>a;
+    cout<<"enter the dimenssion of coulmn"<<endl;
+    cin>>b;
+    for (int i = 1; i <=a; i++)
+    {
+        for (int j = 1; j <=b; j++)
+        {
+            cout<<"*";
+        }
+
+
+        cout<<endl;
+
+    }
+    return 0;
+}
