2677d7201e09de4dcfe4a8aad688c71f093f0241
Aayush0169 <rajaayush124212@gmail.com>
2024-03-17 09:33:56 +0530

    First Commit

---
+++ PATTERNS/soliDsquarE.cpp
@@ -0,0 +1,22 @@
+//Print the Solid Square  pattern
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"enter the dimenssion of the square:"<<endl;
+    cin>>n;
+
+    cout<<"here is the square:"<<endl;
+    for (int i = 1; i <=n; i++)
+    {
+       for (int j = 1; j <=n; j++)
+       {
+      cout<<"*";
+       }
+       
+       cout<<endl;
+    }
+    
+return 0;
+}
