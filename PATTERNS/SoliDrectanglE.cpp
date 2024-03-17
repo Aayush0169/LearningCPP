d0cfb716e1084b15c926cfa89d2f161c8fcb7dbd
Aayush0169 <rajaayush124212@gmail.com>
2024-03-17 09:21:50 +0530

    First Commit

---
+++ PATTERNS/SoliDrectanglE.cpp
@@ -0,0 +1,20 @@
+//Print the solid reactangle pattern
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i,j;
+
+    cout<<"here is the Pattern:"<<endl;
+    for(int i=1;i<=2;i++)
+    {
+        for(int j=1;j<=3;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+   
+return 0;
+}
