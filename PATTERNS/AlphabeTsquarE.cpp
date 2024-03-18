e44e8a5fd256e4459e8807322cb2843f6f3e7393
Aayush0169 <rajaayush124212@gmail.com>
2024-03-18 19:13:45 +0530

    First Commit

---
+++ PATTERNS/AlphabeTsquarE.cpp
@@ -0,0 +1,20 @@
+//Print the alphaber square pattern
+#include<iostream>
+using namespace std;
+int main ()
+{
+    
+
+    cout<<"here is the Pattern:"<<endl;
+    for(int i=1;i<=4;i++)
+    {
+        for(char j='A';j<='D';j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+   
+return 0;
+}
