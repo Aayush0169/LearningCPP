abff83f55c773cfc159aecd236c87610022dd522
Aayush0169 <rajaayush124212@gmail.com>
2024-03-18 15:34:57 +0530

    First Commit

---
+++ PATTERNS/NumbeRsquarE.cpp
@@ -0,0 +1,21 @@
+//Print the number square pattern
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    
+
+    cout<<"here is the Pattern:"<<endl;
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=1;j<=4;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+   
+return 0;
+}
