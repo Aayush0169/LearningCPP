3cf5d7502ad35a7ee0a717330ec44b179a90aad7
Aayush0169 <rajaayush124212@gmail.com>
2024-03-19 18:46:40 +0530

    First Commit

--- PATTERNS2/CrosS.cpp
+++ PATTERNS2/CrosS.cpp
@@ -6,19 +6,18 @@ int main() {
     int input; 
     cout<<"Enter the length of the cross(should be an odd number for better result):"<<endl;
     cin>>input;
-    // Size of the cross pattern, should be an odd number
-    int mid = input / 2; // Middle index for the cross
-
+    
+    int mid = input / 2; 
     for (int i = 0; i < input; ++i) {
         for (int j = 0; j < input; ++j) {
             // Print '*' only at the diagonals
             if (i == j || i + j == input - 1) {
                 cout << "* ";
             } else {
-                cout << "  "; // Print spaces elsewhere
+                cout << "  "; 
             }
         }
-        cout <<endl; // Newline after each row
+        cout <<endl; 
     }
 
     return 0;
