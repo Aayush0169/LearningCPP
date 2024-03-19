f0c58267ab212fc7713e2c9ac5fb275b83377dbd
Aayush0169 <rajaayush124212@gmail.com>
2024-03-19 18:47:52 +0530

    First Commit

--- PATTERNS2/CrosS.cpp
+++ PATTERNS2/CrosS.cpp
@@ -10,7 +10,7 @@ int main() {
     int mid = input / 2; 
     for (int i = 0; i < input; ++i) {
         for (int j = 0; j < input; ++j) {
-            // Print '*' only at the diagonals
+            
             if (i == j || i + j == input - 1) {
                 cout << "* ";
             } else {
