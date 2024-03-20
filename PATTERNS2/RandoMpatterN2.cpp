44b5f018b3a842768697053860e094d8d24fe270
Aayush0169 <rajaayush124212@gmail.com>
2024-03-20 21:38:09 +0530

    First Commit

---
+++ PATTERNS2/RandoMpatterN2.cpp
@@ -0,0 +1,34 @@
+/*
+Print the following pattern
+Input : n = 4
+Output :
+ A
+ A B
+ A B C
+A B C D
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the height of pattern:"<<endl;
+    cin>>n;
+
+    for (int i = 1; i <= n; ++i)
+     {
+        
+        for (int j = 1; j <= n - i; ++j) 
+        {
+            cout << " ";
+        }
+        
+        for (char letter = 'A'; letter < 'A' + i; ++letter)
+         {
+            cout << letter << " ";
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
