1ba6b7a69ff920d1d5e50c887f64d7b776faf922
Aayush0169 <rajaayush124212@gmail.com>
2024-03-23 21:44:02 +0530

    First Commit

---
+++ POINTERS/SuMusinGpointeR.cpp
@@ -0,0 +1,17 @@
+/*Write a program to find the sum of two numbers using pointers*/
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1,num2,sum;
+    int *point1 =&num1;
+    int *point2 =&num2;
+    cout<<"Enter the first number:"<<endl;
+    cin>>*point1;
+    cout<<"Enter the second number:"<<endl;
+    cin>>*point2;
+    sum=(*point1)+(*point2);
+    cout<<"The Sum is "<<sum<<endl;
+    return 0;
+  
+}
