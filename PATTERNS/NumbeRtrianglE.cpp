838352352b59416fab1c5d9f62b8fea05c4343ef
Aayush0169 <rajaayush124212@gmail.com>
2024-03-27 22:05:21 +0530

    First Commit

--- PATTERNS/NumbeRtrianglE.cpp
+++ PATTERNS/NumbeRtrianglE.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 
 int main() {
- int n;
+ int num;
 cout<<"Enter the number of rows:"<<endl;
-cin>>n;
+cin>>num;
 
-for(int i=1;i<=n;i++) 
+for(int i=1;i<=num;i++) 
 { for(int j=1;j<=i;j++)
  { cout<<j;
 
