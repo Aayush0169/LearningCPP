963a90ae66643d8909db452799f2b82dd4a4759f
Aayush0169 <rajaayush124212@gmail.com>
2024-03-31 00:48:47 +0530

    First CommitFirst Commit

--- ARRAYS/findinGelemenT.cpp
+++ ARRAYS/findinGelemenT.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main (){
     int array []={12,15,14,8,15,56,85,9};
-    int search;
+    int x;
     cout<<"Enter the number/elemt you want to find:-"<<endl;
-    cin>>search;
+    cin>>x;
     int result= -1;
    int size=  sizeof(array)/sizeof(array[2]);
     for(int i=0;i<size;i++)
-    if(array[i]==search)
+    if(array[i]==x)
     {
         result=i;
     }
