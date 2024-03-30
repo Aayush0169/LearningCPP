7cf78c5f515484335e6b92a149809ca32b614028
Aayush0169 <rajaayush124212@gmail.com>
2024-03-31 00:43:05 +0530

    First Commit

--- ARRAYS/maximuMelemenT.cpp
+++ ARRAYS/maximuMelemenT.cpp
@@ -2,26 +2,23 @@
 this proggramme is for the  finding max element in an array
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int arr [10];
-cout<<"Enter elemrnts in array:";
+int main() {
+    int arr[10];
+    cout << "Enter elements in the array: ";
 
-for (int i = 0; i <10; i++)
-{
-    cin>>arr[i];
-}
-int max=arr[0];
-for(int j=1;j<10;j++)
-{
-    if (arr[j]>max);
-    {
-       max=arr[j];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
     }
-    
-}
-cout<<"the MAx number in this array is:"<<endl;
-cout<<max<<endl;
-return 0;
+
+    int max = arr[0];
+    for (int j = 1; j < 10; j++) {
+        if (arr[j] > max) { 
+            max = arr[j];
+        }
+    }
+
+    cout << "The maximum number in this array is: " << max << endl;
+    return 0;
 }
