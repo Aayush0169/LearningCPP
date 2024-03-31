e48307cd4162573aa6285f4ff696d913c9dfefa8
Aayush0169 <rajaayush124212@gmail.com>
2024-04-01 00:45:14 +0530

    First Commit

---
+++ ARRAYS/dynamicArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int number[11];
+  cout<<"Enter the number:";
+  for(int i=0;i<=10;i++)
+  {
+    cin>>number[i];
+  }
+  for(int i=0;i<=10;i++)
+  {
+cout<<number[i]<<" ";
+  }
+
+    
+return 0;
+}
