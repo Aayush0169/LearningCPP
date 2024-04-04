9bccf25a1dbb974858fc1d85cf17bdad9c444652
Aayush0169 <rajaayush124212@gmail.com>
2024-04-04 14:23:07 +0530

    Fixing Bug

--- PATTERNS2/BinarYtrianglE.cpp
+++ PATTERNS2/BinarYtrianglE.cpp
@@ -1,23 +1,27 @@
 //Print the given pattern(BINARY TRIANGLE)
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int input;
-    cout << "Enter the number of rows : ";
-    cin >> input;
+int main (){
+    int n;
+    cout<<"Enter the numbers of roes: "<<endl;
+    cin>>n;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++){
 
-    int value = 1;
-    for(int i = 0; i <= input; ++i) {
-        for(int j = 0; j <= i; ++j) {
-            cout << value << " ";
-            value = !value; 
-        }
-        if(i % 2 == 0) {
-            value = 1;
-        } else { 
-            value = 0;
-        }
-        cout << endl;
+            if ((i+j)%2==0)
+            {
+                cout<<"1";
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"0";
+                cout<<" ";
+            }
+            
+        }cout<<" "<<endl;
     }
-    return 0;
+   
 }
