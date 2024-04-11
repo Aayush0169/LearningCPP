193b10228da9de67f27b697512649e93e15112ae
Aayush0169 <rajaayush124212@gmail.com>
2024-04-12 00:15:23 +0530

    First Commit

--- VECTORS/declarinGvectoR.cpp
+++ VECTORS/declarinGvectoR.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
    vector<int> vec ={12,16,14,15,16,17,18,19,20,};
-   cout<<"size is :"<<vec.size()<<endl;
+   cout<<"size is :"<<vec.size()<<endl<<endl;
    cout<<"capacity is :"<<vec.capacity()<<endl<<endl;
    cout<<"The elements are:"<<endl;
    for(int elements : vec){
