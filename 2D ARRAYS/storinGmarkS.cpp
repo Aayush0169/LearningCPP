3583074c4e696f76d624f8dceff8740f271d82cd
Aayush0169 <rajaayush124212@gmail.com>
2024-04-12 20:51:58 +0530

    First Commit

--- 2D ARRAYS/storinGmarkS.cpp
+++ 2D ARRAYS/storinGmarkS.cpp
@@ -2,31 +2,24 @@
 using namespace std;
 
 int main() {
-    // Assuming the maximum number of marks is 100
-    const int MAX_MARKS = 100;
-    const int NUM_STUDENTS = 4;
+
+   
+    int STUDENTS = 4;
     
-    // 2D array to store roll number and marks
-    // First column for roll numbers, second column for marks
-    int studentData[NUM_STUDENTS][2];
+   
+    int studentData[STUDENTS][2];
     
     // Input data from user
-    for(int i = 0; i < NUM_STUDENTS; ++i) {
+    for(int i = 0; i < STUDENTS; ++i) {
         cout << "Enter roll number for student " << i + 1 << ": ";
         cin >> studentData[i][0];
         cout << "Enter marks for student " << i + 1 << ": ";
         cin >> studentData[i][1];
-        
-        // Validate marks
-        if(studentData[i][1] < 0 || studentData[i][1] > MAX_MARKS) {
-            cout << "Invalid marks! Please enter a value between 0 and " << MAX_MARKS << "." << endl;
-            --i; // Decrement i to retake input for the same student
-        }
     }
     
     // Display the matrix
     cout << "\nRoll Number\tMarks" << endl;
-    for(int i = 0; i < NUM_STUDENTS; ++i) {
+    for(int i = 0; i < STUDENTS; ++i) {
         cout << studentData[i][0] << "\t\t" << studentData[i][1] << endl;
     }
     
