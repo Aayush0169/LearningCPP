f8acfd4a0785a6c015672efacdc149503755a2b9
Aayush0169 <rajaayush124212@gmail.com>
2024-05-10 23:30:39 +0530

    First Commit

--- CODNITIONAL OPERATOR/eveNnumberSabetweeN2numberS.cpp
+++ CODNITIONAL OPERATOR/eveNnumberSabetweeN2numberS.cpp
@@ -5,25 +5,22 @@ int main() {
     int start, end;
     int evenCount = 0; // Initialize the count to zero
 
-    // Input the start and end values
     cout << "Enter the start number: ";
     cin >> start;
     cout << "Enter the end number: ";
     cin >> end;
 
-    // Adjust start number if it is odd
     if (start % 2 != 0) {
         start++;
     }
 
-    // Print even numbers in the range and update the count
+   
     cout << "Even numbers between " << start << " and " << end << " are:" << endl;
     for (int i = start; i <= end; i += 2) {
         cout << i << " ";
-        evenCount++; // Increment the count
+        evenCount++; 
     }
 
-    // Print the total count
     cout << "\nTotal even numbers: " << evenCount << endl;
 
     return 0;
